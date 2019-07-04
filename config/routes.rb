b1cac4616375cbbee32e7956d5b5e18eeb0cc3f6
Yusuke <yusuke.tutorial@gmail.com>
2019-07-04 20:14:47 +0000

    4章終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home',"static_pages#home"
+  #get 'static_pages/home',"static_pages#home"
 
   get 'static_pages/about','static_pages#about'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,9 +6,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get about" do
